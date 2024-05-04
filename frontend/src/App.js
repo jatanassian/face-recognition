04f9dbe553ed24c07136ffa94a729ef875bccc77
Julien Atanassian <julien.atanassian@gmail.com>
2024-05-04 15:26:09 -0400

    add logo component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Navigation />
-        {/* <Logo />
-		<ImageLinkForm />
+const App = () => {
+  return (
+    <div className='App'>
+      <Navigation />
+      <Logo />
+      {/* <ImageLinkForm />
 		<FaceRecognition /> */}
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;

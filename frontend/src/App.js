50fccb48781c950d6d6bf2ab5cb5acc944552818
Julien Atanassian <julien.atanassian@gmail.com>
2024-05-04 16:05:01 -0400

    add particles bg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/image-link-form/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import ParticlesBg from 'particles-bg';
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
+      <ParticlesBg type='cobweb' bg={true} color='#f5f5f5' num={200} />
       <Navigation />
       <Logo />
       <Rank />

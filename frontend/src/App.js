e70917e4e38d405b4826fef1cbf33b51d3b19a66
Julien Atanassian <julien.atanassian@gmail.com>
2024-05-04 15:49:59 -0400

    add rank component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/image-link-form/ImageLinkForm';
+import Rank from './components/rank/Rank';
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className='App'>
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>

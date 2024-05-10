780a3c198c490a2a8802e847bd0af892f7aa9a12
Julien Atanassian <julien.atanassian@gmail.com>
2024-05-10 09:38:24 -0400

    add input state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,22 @@ import ImageLinkForm from './components/image-link-form/ImageLinkForm';
 import Rank from './components/rank/Rank';
 import ParticlesBg from 'particles-bg';
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [input, setInput] = useState('');
+
+  const onInputChange = e => {
+    console.log(e.target.value);
+  };
+
   return (
     <div className='App'>
       <ParticlesBg type='cobweb' bg={true} color='#f5f5f5' num={200} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} />
       {/* <FaceRecognition /> */}
     </div>
   );

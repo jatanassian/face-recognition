2858ffb357b10086a03e2c6dc25caaeabd3e3b9a
Julien Atanassian <julien.atanassian@gmail.com>
2024-05-11 11:33:31 -0400

    cleanup

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -2,11 +2,11 @@ const Register = ({ onRouteChange }) => {
   return (
     <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
       <main className='pa4 black-80'>
-        <form className='measure'>
+        <div className='measure'>
           <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
             <legend className='f1 fw6 ph0 mh0'>Register</legend>
             <div className='mt3'>
-              <label className='db fw6 lh-copy f6' for='name'>
+              <label className='db fw6 lh-copy f6' htmlFor='name'>
                 Name
               </label>
               <input
@@ -17,7 +17,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
             <div className='mt3'>
-              <label className='db fw6 lh-copy f6' for='email-address'>
+              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
                 Name
               </label>
               <input
@@ -28,7 +28,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
             <div className='mv3'>
-              <label className='db fw6 lh-copy f6' for='password'>
+              <label className='db fw6 lh-copy f6' htmlFor='password'>
                 Password
               </label>
               <input
@@ -39,7 +39,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
           </fieldset>
-          <div className=''>
+          <div>
             <input
               className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
               type='submit'
@@ -47,7 +47,7 @@ const Register = ({ onRouteChange }) => {
               onClick={() => onRouteChange('home')}
             />
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
--- frontend/src/components/sign-in/SignIn.jsx
+++ frontend/src/components/sign-in/SignIn.jsx
@@ -2,11 +2,11 @@ const SignIn = ({ onRouteChange }) => {
   return (
     <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
       <main className='pa4 black-80'>
-        <form className='measure'>
+        <div className='measure'>
           <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
             <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
             <div className='mt3'>
-              <label className='db fw6 lh-copy f6' for='email-address'>
+              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
                 Email
               </label>
               <input
@@ -14,10 +14,11 @@ const SignIn = ({ onRouteChange }) => {
                 className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
                 type='email'
                 name='email-address'
+                autoComplete='off'
               />
             </div>
             <div className='mv3'>
-              <label className='db fw6 lh-copy f6' for='password'>
+              <label className='db fw6 lh-copy f6' htmlFor='password'>
                 Password
               </label>
               <input
@@ -25,10 +26,11 @@ const SignIn = ({ onRouteChange }) => {
                 className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
                 type='password'
                 name='password'
+                autoComplete='off'
               />
             </div>
           </fieldset>
-          <div className=''>
+          <div>
             <input
               className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
               type='submit'
@@ -44,7 +46,7 @@ const SignIn = ({ onRouteChange }) => {
               Register
             </p>
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );

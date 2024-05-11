cb0ec55105d6891dea36ea81b8551fb4f8dc5e01
Julien Atanassian <julien.atanassian@gmail.com>
2024-05-11 08:52:07 -0400

    implement routing with sign-in and home pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/image-link-form/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import SignIn from './components/sign-in/SignIn';
 import FaceRecognition from './components/face-recognition/FaceRecognition';
 import ParticlesBg from 'particles-bg';
 import './App.css';
@@ -13,6 +14,7 @@ const App = () => {
   const [input, setInput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [box, setBox] = useState({});
+  const [route, setRoute] = useState('sign-in');
 
   const calculateFaceLocation = data => {
     const faceArea = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -75,14 +77,27 @@ const App = () => {
       .catch(error => console.log('error', error));
   };
 
+  const onRouteChange = route => {
+    setRoute(route);
+  };
+
   return (
     <div className='App'>
       <ParticlesBg type='cobweb' bg={true} color='#f5f5f5' num={200} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onSubmit} />
-      <FaceRecognition imageUrl={imageUrl} box={box} />
+      <Navigation onRouteChange={onRouteChange} />
+      {route === 'sign-in' ? (
+        <SignIn onRouteChange={onRouteChange} />
+      ) : (
+        <>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onSubmit}
+          />
+          <FaceRecognition imageUrl={imageUrl} box={box} />
+        </>
+      )}
     </div>
   );
 };
--- frontend/src/components/sign-in/SignIn.jsx
+++ frontend/src/components/sign-in/SignIn.jsx
@@ -1,4 +1,4 @@
-const SignIn = () => {
+const SignIn = ({ onRouteChange }) => {
   return (
     <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
       <main className='pa4 black-80'>
@@ -33,6 +33,7 @@ const SignIn = () => {
               className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
               type='submit'
               value='Sign in'
+              onClick={() => onRouteChange('home')}
             />
           </div>
           <div className='lh-copy mt3'>

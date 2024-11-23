fcc46bfa6d0fe0ae6d492cdaf539908c05045f54
Julien Atanassian <julien.atanassian@gmail.com>
2024-11-23 18:03:26 -0500

    add connectionString

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
+    connectionString: process.env.DB_URL,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,

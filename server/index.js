3c375e5987e1bb1827e0c73d1bad9f6b9695c4db
Julien Atanassian <julien.atanassian@gmail.com>
2024-11-24 13:34:29 -0500

    remove connectionString

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DB_URL,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
